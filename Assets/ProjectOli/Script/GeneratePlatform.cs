81224f43376dccf9563298f6e9390b74546abba5
Technifutur <600@technifutur.be>
2018-01-30 12:03:37 +0100

    platform random height ok

---
+++ Assets/ProjectOli/Script/GeneratePlatform.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GeneratePlatform : MonoBehaviour
+{
+    public float spaceBetweenPlatforms = 5;
+    public float playerHeight;
+    public float platformHeight = 5;
+    public GameObject platform;
+    public GameObject player;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        for (int i = 0; i < 15; i++)
+        {
+            Instantiate(platform, new Vector3(((float)Random.value * 20) - 10, (float)platformHeight, 0f), Quaternion.identity);
+            platformHeight += spaceBetweenPlatforms;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(platformHeight - player.transform.position.y <=15f || player.transform.position.y >150f)
+        {
+            Instantiate(platform, new Vector3(((float)Random.value * 20) - 10, (float)platformHeight, 0f), Quaternion.identity);
+            platformHeight += spaceBetweenPlatforms;
+            spaceBetweenPlatforms += 0.2f;
+        }
+	}
+}
